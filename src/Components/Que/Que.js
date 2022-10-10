9c3344135cb9ac9da1f95cc04f41d84080b9058e
RafatSh-hash <73437594+RafatSh-hash@users.noreply.github.com>
2022-10-11 03:29:20 +0600

    added toast

--- src/Components/Que/Que.js
+++ src/Components/Que/Que.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Que = ({ que }) => {
   const { question, options, correctAnswer } = que;
+
+  const notify = () => toast(correctAnswer);
 
   return (
     <div className="">
       <div className="w-1/2 h-66 mx-auto leading-9 shadow-gray-600 shadow-lg mt-10 border-2 border-slate-500 p-4 rounded-2xl">
         <div className="flex justify-between">
           <p className="font-bold w-[80%]">{question}</p>
-          <button className="w-[20%]">
+          <button onClick={notify} className="w-[20%]">
             <FontAwesomeIcon icon={faEye} />
           </button>
+          <ToastContainer />
         </div>
         <input className="font-semibold" name="a" type="radio" />
         <label className="font-semibold" htmlFor="">
