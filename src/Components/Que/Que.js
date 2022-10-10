66e87702b21f3e0eceb03579686413895e8d4cd4
RafatSh-hash <73437594+RafatSh-hash@users.noreply.github.com>
2022-10-11 04:49:19 +0600

    Added Alerts

--- src/Components/Que/Que.js
+++ src/Components/Que/Que.js
@@ -3,12 +3,18 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEye } from "@fortawesome/free-solid-svg-icons";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Swal from "sweetalert2";
 
 const Que = ({ que }) => {
   const { question, options, correctAnswer } = que;
 
   const notify = () => toast(correctAnswer);
-
+  const onChangeValue = (e) => {
+    const selected = e.target.value;
+    if (selected === correctAnswer) {
+      Swal.fire("Correct Answer!!!");
+    }
+  };
   return (
     <div className="">
       <div className="w-1/2 h-66 mx-auto leading-9 shadow-gray-600 shadow-lg mt-10 border-2 border-slate-500 p-4 rounded-2xl">
@@ -19,22 +25,55 @@ const Que = ({ que }) => {
           </button>
           <ToastContainer />
         </div>
-        <input className="font-semibold" name="a" type="radio" />
+        <input
+          className="font-semibold"
+          name="a"
+          value={options[0]}
+          onChange={(e) => {
+            onChangeValue(e);
+          }}
+          type="radio"
+        />
         <label className="font-semibold" htmlFor="">
           <span> {options[0]}</span>
         </label>
+
         <br />
-        <input className="font-semibold" name="a" type="radio" />
+        <input
+          className="font-semibold"
+          name="a"
+          value={options[1]}
+          onChange={(e) => {
+            onChangeValue(e);
+          }}
+          type="radio"
+        />
         <label className="font-semibold" htmlFor="">
           <span> {options[1]}</span>
         </label>
         <br />
-        <input className="font-semibold" name="a" type="radio" />
+        <input
+          className="font-semibold"
+          name="a"
+          value={options[2]}
+          onChange={(e) => {
+            onChangeValue(e);
+          }}
+          type="radio"
+        />
         <label className="font-semibold" htmlFor="">
           <span> {options[2]}</span>
         </label>
         <br />
-        <input className="font-semibold" name="a" type="radio" />
+        <input
+          className="font-semibold"
+          name="a"
+          value={options[3]}
+          onChange={(e) => {
+            onChangeValue(e);
+          }}
+          type="radio"
+        />
         <label className="font-semibold" htmlFor="">
           <span> {options[3]}</span>
         </label>
